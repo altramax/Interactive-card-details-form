6393b4c0b42cf28373ac83fda4fda46f6301815d
altramax <ajayi_ezekiel@yahoo.com>
2022-12-18 15:02:37 +0100

    U.I completed

---
+++ card.js
@@ -0,0 +1,6 @@
+const form = document.getElementById('card-inputfield');
+const thankYou = document.getElementById('thank-you');
+
+form.style.display = 'none';
+thankYou.style.display = "flex";
+// document.getElementById(id).style.property = new style
