63be9798b3069cfe04b03a40c9b47e34322d0791
altramax <ajayi_ezekiel@yahoo.com>
2022-12-20 22:55:37 +0100

    Updated

--- card.js
+++ card.js
@@ -1,6 +1,62 @@
+"use strict"
 const form = document.getElementById('card-inputfield');
 const thankYou = document.getElementById('thank-you');
+const backText = document.getElementById('back-card-text');
+const cardNumber = document.getElementById('card-number');
+const formCardNum = document.getElementById('form-card-number');
+const cardName = document.getElementById('card-name');
+const expDate = document.getElementById('exp-date');
+const button = document.getElementById('check');
 
-form.style.display = 'none';
-thankYou.style.display = "flex";
+
 // document.getElementById(id).style.property = new style
+// form.style.display = 'none';
+// thankYou.style.display = "flex";
+
+//    to stop the input at specific value 
+    //  function stop(){
+    //   if(formCardNum.value =)
+    //  }
+
+    let letters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ","];
+
+   // To receive only number 
+   formCardNum.addEventListener("keyup", function(e){  
+
+       for(const num of formCardNum.value){
+           if(!letters.includes(num)){
+               formCardNum.value = formCardNum.value.replace(num, "")
+               console.log(num);          
+            }    
+        }  
+        function maxInputs(){
+            if (formCardNum.value.length > formCardNum.maxLength){
+                formCardNum.value = formCardNum.value.slice(0, formCardNum.maxLength); 
+             }
+         }
+
+
+        //  if(formCardNum.value.length[e.value] === 5){
+        //     //  function split(){
+        //    console.log(   formCardNum.value.match(/.{1,4}/g).replace(',', " "));
+           
+        //     //  }
+        //  }
+         
+        })
+
+
+
+
+
+
+button.addEventListener("click", function(){
+
+cardNumber.textContent = formCardNum.value;
+
+    console.log(typeof formCardNum.value);
+
+
+
+
+})  
