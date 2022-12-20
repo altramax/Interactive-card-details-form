a98303e4018653eacf661453f08bcd71210a345c
altramax <ajayi_ezekiel@yahoo.com>
2022-12-20 23:39:05 +0100

    update

--- card.js
+++ card.js
@@ -49,6 +49,7 @@ const button = document.getElementById('check');
 
         formCardNum.addEventListener('input', function (e) {
             e.target.value = e.target.value.replace(/[^\dA-Z]/g, '').replace(/(.{4})/g, '$1 ').trim();
+            console.log(e.target.value);
             cardNumber.textContent = e.target.value;
           });
 
